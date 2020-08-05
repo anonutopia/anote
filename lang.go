8becc49f541661f258a981d0708e047e9b988d73
Pragmatic <cryptopragmatic@protonmail.com>
2020-08-05 20:26:13 +0200

    fixed bug

--- lang.go
+++ lang.go
@@ -3,7 +3,11 @@ package main
 import ui18n "github.com/unknwon/i18n"
 
 func trUser(user *User, message string) string {
-	return ui18n.Tr(user.Language, message)
+	if len(user.Language) > 0 {
+		return ui18n.Tr(user.Language, message)
+	} else {
+		return ui18n.Tr(lang, message)
+	}
 }
 
 func trGroup(groupID int, message string) string {
