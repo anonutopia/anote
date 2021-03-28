cc564763356f42e833147714f7b9d20f4cc3354c
Pragmatic <cryptopragmatic@protonmail.com>
2021-03-28 03:55:43 +0200

    waves stuff

--- helpers.go
+++ helpers.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/wavesplatform/gowaves/pkg/client"
+	"github.com/wavesplatform/gowaves/pkg/crypto"
+	"github.com/wavesplatform/gowaves/pkg/proto"
+)
+
 func stringInSlice(a string, list []string) bool {
 	for _, b := range list {
 		if b == a {
@@ -7,4 +18,71 @@ func stringInSlice(a string, list []string) bool {
 		}
 	}
 	return false
+}
+
+func sendAsset(amount uint64, assetId string, recipient string) error {
+	var assetBytes []byte
+
+	// Create sender's public key from BASE58 string
+	sender, err := crypto.NewPublicKeyFromBase58(conf.PublicKey)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// Create sender's private key from BASE58 string
+	sk, err := crypto.NewSecretKeyFromBase58(conf.PrivateKey)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// Current time in milliseconds
+	ts := time.Now().Unix() * 1000
+
+	if len(assetId) > 0 {
+		assetBytes = crypto.MustBytesFromBase58(assetId)
+	} else {
+		assetBytes = []byte{}
+	}
+
+	asset, err := proto.NewOptionalAssetFromBytes(assetBytes)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	rec, err := proto.NewAddressFromString(recipient)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	tr := proto.NewUnsignedTransferWithSig(sender, *asset, *asset, uint64(ts), amount, 100000, proto.Recipient{Address: &rec}, nil)
+
+	err = tr.Sign(proto.MainNetScheme, sk)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// Create new HTTP client to send the transaction to public TestNet nodes
+	client, err := client.NewClient(client.Options{BaseUrl: WavesNodeURL, Client: &http.Client{}})
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// Context to cancel the request execution on timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// // Send the transaction to the network
+	_, err = client.Transactions.Broadcast(ctx, tr)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
 }
